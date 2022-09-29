3f0bbb8c2ecb7003e56b67b880b8d35559374ca9
albades <albadevdes@gmail.com>
2022-09-29 20:34:38 -0300

    calculadora em javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+var n1 = document.getElementById("n1")
+var n2 = document.getElementById("n2")
+var res = document.getElementById("res")
+
+function soma (){
+    res.innerHTML = parseInt(n.value) + parseInt(n2.value)
+    console.log(res)
+}
+
+function subtração (){
+    res.innerHTML = parseInt(n1) + parseInt(n2)
+    console.log(res)
+}
+
